aab733d69313277e8f6c614a3d8bf844c091d23e
Samba Bery KANE <sambabery.kane@gmail.com>
2021-12-15 15:44:42 +0000

    refactoring du code pour creer une fonction qui permet de creer une carte

--- app.js
+++ app.js
@@ -6,83 +6,14 @@ const LISTE_IDEES = [
     suggestion: "juste une proposition pour les heures de cours",
     statut: false,
   },
-  {
-    id: 2,
-    titre: "Brief",
-    suggestion: "juste une proposition pour les validations",
-    statut: true,
-  },
-  {
-    id: 3,
-    titre: "Planning",
-    suggestion: "juste une proposition pour fetes ",
-    statut: false,
-  },
-  {
-    id: 4,
-    titre: "Cotisations",
-    suggestion: "Il faut supprimer les cotisations",
-    statut: false,
-  },
 ]
 
 // RECUPERATIONS DES ELEMENTS DOM
 const propositionElement = document.getElementById("propositions")
 const ideeForm = document.querySelector("form")
 
-// RECUPERATION DES INFORMAIONS DU FORMULAIRE
-
-ideeForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const inputTitre = document.querySelector("input#titre")
-  const inputSuggestion = document.querySelector("textarea#suggestion")
-
-  // Récupération des informations saisies
-  const titreSaisi = inputTitre.value
-  const suggestionSaisi = inputSuggestion.value
-
-  // mettre les informations sous forme
-  const nouvelleIdee = {
-    id: 5,
-    titre: titreSaisi,
-    suggestion: suggestionSaisi,
-    statut: false,
-  }
-
-  // Ajout de la nouvelle idée au niveau du tableau idées
-  LISTE_IDEES.push(nouvelleIdee)
-
-  // on vide les champs
-  inputTitre.value = ""
-  inputSuggestion.value = ""
-
-  //AJOUT DE LA NOUVELLE IDEE AU NIVEAU DE LA PAGE
-  const divCard = document.createElement("div")
-  divCard.classList.add("card")
-  divCard.classList.add("m-2")
-  divCard.classList.add("col-3")
-  divCard.style.width = "22rem"
-
-  const divCardBody = document.createElement("div")
-  divCardBody.classList.add("card-body")
-
-  const cardTitle = document.createElement("h5")
-  cardTitle.classList.add("card-title")
-
-  const cardDescription = document.createElement("p")
-  cardDescription.classList.add("card-text")
-
-  cardTitle.textContent = nouvelleIdee.titre
-  cardDescription.textContent = nouvelleIdee.suggestion
-
-  divCardBody.appendChild(cardTitle)
-  divCardBody.appendChild(cardDescription)
-  divCard.appendChild(divCardBody)
-  propositionElement.appendChild(divCard)
-})
-
-// AFFICHAGE DE LA DES IDEES
-LISTE_IDEES.forEach((idee) => {
+// NOS FONCTIONS
+const creerUneCarte = (idee) => {
   const divCard = document.createElement("div")
   divCard.classList.add("card")
   divCard.classList.add("m-2")
@@ -105,4 +36,44 @@ LISTE_IDEES.forEach((idee) => {
   divCardBody.appendChild(cardDescription)
   divCard.appendChild(divCardBody)
   propositionElement.appendChild(divCard)
+}
+
+// RECUPERATION DES INFORMAIONS DU FORMULAIRE
+
+ideeForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const inputTitre = document.querySelector("input#titre")
+  const inputSuggestion = document.querySelector("textarea#suggestion")
+
+  // Récupération des informations saisies
+  const titreSaisi = inputTitre.value
+  const suggestionSaisi = inputSuggestion.value
+
+  if (titreSaisi.trim().length < 5 || suggestionSaisi.trim().length < 10) {
+    alert("Merci de saisir des informations correctes")
+    return
+  }
+
+  // mettre les informations sous forme
+  const nouvelleIdee = {
+    id: 5,
+    titre: titreSaisi,
+    suggestion: suggestionSaisi,
+    statut: false,
+  }
+
+  // Ajout de la nouvelle idée au niveau du tableau idées
+  LISTE_IDEES.push(nouvelleIdee)
+
+  // on vide les champs
+  inputTitre.value = ""
+  inputSuggestion.value = ""
+
+  //AJOUT DE LA NOUVELLE IDEE AU NIVEAU DE LA PAGE
+  creerUneCarte(nouvelleIdee)
+})
+
+// AFFICHAGE DE LA DES IDEES
+LISTE_IDEES.forEach((idee) => {
+  creerUneCarte(idee)
 })
