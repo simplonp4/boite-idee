84214eb9c77ed076d4cd2eb7f63e39e0dfac377c
Samba Bery KANE <sambabery.kane@gmail.com>
2021-12-16 10:20:26 +0000

    amélioration de la carte pour les idées

--- app.js
+++ app.js
@@ -11,30 +11,31 @@ const inputSuggestion = document.querySelector("textarea#suggestion")
 
 // NOS FONCTIONS
 const creerUneCarte = (idee) => {
-  const divCard = document.createElement("div")
-  divCard.classList.add("card")
-  divCard.classList.add("animate__animated")
-  divCard.classList.add("animate__bounce")
-  divCard.classList.add("m-2")
-  divCard.classList.add("col-3")
-  divCard.style.width = "22rem"
-
-  const divCardBody = document.createElement("div")
-  divCardBody.classList.add("card-body")
-
-  const cardTitle = document.createElement("h5")
-  cardTitle.classList.add("card-title")
-
-  const cardDescription = document.createElement("p")
-  cardDescription.classList.add("card-text")
-
-  cardTitle.textContent = idee.titre
-  cardDescription.textContent = idee.suggestion
-
-  divCardBody.appendChild(cardTitle)
-  divCardBody.appendChild(cardDescription)
-  divCard.appendChild(divCardBody)
-  propositionElement.appendChild(divCard)
+  propositionElement.insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div class="card me-1" style="width: 18rem">
+      <div class="card-body">
+          <h5 class="card-title fw-bold">${idee.titre}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">
+              approuvée / réfusée
+          </h6>
+          <p class="card-text">${idee.suggestion}
+          </p>
+          <div class="d-flex justify-content-between">
+              <i
+                  class="bi bi-check-circle text-success card-link"
+                  style="font-size: 2rem"
+              ></i>
+              <i
+                  class="bi bi-x-circle card-link"
+                  style="font-size: 2rem; color: #ce0033"
+              ></i>
+          </div>
+      </div>
+  </div>
+  `
+  )
 }
 
 // VERIFICATION DES MOTS SAISIS
